a65b671ad5e226768dd1e04d0f0c13016324632e
Phodcharaphon Sukonsakun <patt502090@hotmail.com>
2025-05-02 15:05:59 +0700

    feat: add navigation-menu package and update social media links in Footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,42 +8,42 @@ const Footer = () => {
         <div className="flex justify-center space-x-2 mb-4">
           {/* Facebook */}
           <a
-            href="#!"
+            href="https://www.facebook.com/"
             className="inline-flex items-center justify-center w-10 h-10 rounded-full border border-white text-white hover:bg-white hover:text-gray-800 transition"
           >
             <i className="fab fa-facebook-f"></i>
           </a>
-          {/* Twitter */}
+          {/* Twitter or X*/}
           <a
-            href="#!"
+            href="https://x.com/?lang=th"
             className="inline-flex items-center justify-center w-10 h-10 rounded-full border border-white text-white hover:bg-white hover:text-gray-800 transition"
           >
             <i className="fab fa-twitter"></i>
           </a>
           {/* Google */}
           <a
-            href="#!"
+            href="https://www.google.co.th/"
             className="inline-flex items-center justify-center w-10 h-10 rounded-full border border-white text-white hover:bg-white hover:text-gray-800 transition"
           >
             <i className="fab fa-google"></i>
           </a>
           {/* Instagram */}
           <a
-            href="#!"
+            href="https://www.instagram.com/"
             className="inline-flex items-center justify-center w-10 h-10 rounded-full border border-white text-white hover:bg-white hover:text-gray-800 transition"
           >
             <i className="fab fa-instagram"></i>
           </a>
           {/* LinkedIn */}
           <a
-            href="#!"
+            href="https://www.linkedin.com/"
             className="inline-flex items-center justify-center w-10 h-10 rounded-full border border-white text-white hover:bg-white hover:text-gray-800 transition"
           >
             <i className="fab fa-linkedin-in"></i>
           </a>
           {/* GitHub */}
           <a
-            href="#!"
+            href="https://github.com/"
             className="inline-flex items-center justify-center w-10 h-10 rounded-full border border-white text-white hover:bg-white hover:text-gray-800 transition"
           >
             <i className="fab fa-github"></i>
