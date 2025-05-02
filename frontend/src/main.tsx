e61e0f21f92f322e5dc4429039810d7e79a950f2
tanawat <tanawat.bmk@gmail.com>
2025-05-02 11:10:47 +0700

    init

---
+++ frontend/src/main.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './global.css'
+
+import App from './App'
+import { Providers } from './providers'
+
+const root = ReactDOM.createRoot(document.getElementById('root')!)
+
+// Setup MSW mock server in development
+if (process.env.NODE_ENV === 'development') {
+  // Certify MSW's Service Worker is available before start React app.
+  import('../mocks/browser')
+    .then(async ({ worker }) => {
+      return worker.start()
+    }) // Run <App /> when Service Worker is ready to intercept requests.
+    .then(() => {
+      root.render(
+        <React.StrictMode>
+          <Providers>
+            <App />
+          </Providers>
+        </React.StrictMode>,
+      )
+    })
+  // Never setup MSW mock server in production
+} else if (process.env.NODE_ENV === 'production') {
+  root.render(
+    <React.StrictMode>
+      <Providers>
+        <App />
+      </Providers>
+    </React.StrictMode>,
+  )
+}
