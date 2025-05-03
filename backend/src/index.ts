9cd8429aa75232c7cc53fac577d8211f4cd84ade
adithep.b <adithepbaebmueann@gmail.com>
2025-05-03 16:52:05 +0700

    [Update] install Walrus SDK

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/',(request,response,next)=>{
+    console.log('Request received at /api/users');
+});
+
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`);
 })
