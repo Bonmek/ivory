061e9c8c630374779e210120b3df868a460ee476
Adithep Baebmuean <adithepbaebmueann@gmail.com>
2025-05-06 09:28:29 +0000

    [Add] WriteBlob, set blobId in blob attribute and github OAuth

---
+++ backend/src/index.ts
@@ -0,0 +1,109 @@
+import express from "express";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
+import { WalrusClient } from "@mysten/walrus";
+import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
+import multer from "multer";
+import dotenv from "dotenv";
+import cors from "cors";
+import passport from "passport";
+import session from "express-session";
+import "./github/githubAuth";
+import { githubRoutes } from "./github/routes";
+
+
+dotenv.config();
+const upload = multer();
+const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    exposedHeaders: ["Content-Disposition", "Content-Length"],
+  })
+);
+app.use(
+  session({
+    secret: "session-secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(githubRoutes);
+
+const hex = process.env.SUI_Hex || "";
+const secretKey = Uint8Array.from(Buffer.from(hex, "hex"));
+const keypair = Ed25519Keypair.fromSecretKey(secretKey);
+
+const suiClient = new SuiClient({ url: getFullnodeUrl("mainnet") });
+const walrusClient = new WalrusClient({
+  network: "mainnet",
+  suiClient,
+});
+
+app.post("/write-blob", upload.single("file"), async (req, res) => {
+  try {
+    if (!req.file) {
+      res.status(400).json({ error: "No file uploaded" });
+      return;
+    }
+    const fileBuffer = new Uint8Array(req.file.buffer);
+
+    const attributes = req.body.attributes
+      ? typeof req.body.attributes === "string"
+        ? JSON.parse(req.body.attributes)
+        : req.body.attributes
+      : {};
+
+    const { blobId, blobObject } = await walrusClient.writeBlob({
+      blob: fileBuffer,
+      deletable: false,
+      epochs: 1,
+      signer: keypair,
+      attributes: attributes,
+    });
+
+    const blobObjectId =
+      typeof blobObject.id === "string" ? blobObject.id : blobObject.id.id;
+
+    await walrusClient
+      .executeWriteBlobAttributesTransaction({
+        blobObjectId: blobObjectId,
+        signer: keypair,
+        attributes: { blobId: blobId },
+      })
+
+    const attrs = await walrusClient.readBlobAttributes({ blobObjectId });
+
+    res.json({ statusCode: 1, data: { attributes: attrs } });
+  } catch (error) {
+    if (error instanceof Error) {
+      res.json({
+        statusCode: 0,
+        data: {
+          error: {
+            message: error.message,
+            name: error.name,
+            stack: error.stack,
+          },
+        },
+      });
+    } else {
+      res.json({
+        statusCode: 0,
+        data: { error: "Unknown error occurred" },
+      });
+    }
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
+
