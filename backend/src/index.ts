2509403ad8e0e17bd2ee8bdec58e63b5da0251f5
adithep.b <adithepbaebmueann@gmail.com>
2025-05-19 16:49:33 +0700

    [Update] make make backend on docker

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -15,26 +15,23 @@ import passport from "passport";
 import session from "express-session";
 import "./github/githubAuth";
 import { githubRoutes } from "./github/routes";
-import axios from "axios";
-import { GoogleAuth } from "google-auth-library";
+
 import {
   inputWriteBlobScheme,
   inputDeleteBlobScheme,
   inputSetAttributesScheme,
   inputUpdateWriteBlobScheme,
 } from "./schemas/inputScheme";
-import { CloudTasksClient } from '@google-cloud/tasks';
-import { v4 as uuidv4 } from 'uuid';
+import { CloudTasksClient } from "@google-cloud/tasks";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
-const auth = new GoogleAuth({
-  scopes: ["https://www.googleapis.com/auth/cloud-platform"],
-});
+
 const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || '*',
+    origin: process.env.FRONTEND_URL || "*",
     credentials: true,
     exposedHeaders: ["Content-Disposition", "Content-Length"],
   })
@@ -51,9 +48,6 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(githubRoutes);
 
-const tasksClient = new CloudTasksClient();
-const parent = tasksClient.queuePath(process.env.PROJECT_ID || "", process.env.REGION || "", process.env.CLOUD_NAME || "");
-
 const hex = process.env.SUI_Hex || "";
 const secretKey = Uint8Array.from(Buffer.from(hex, "hex"));
 const keypair = Ed25519Keypair.fromSecretKey(secretKey);
@@ -63,6 +57,13 @@ const walrusClient = new WalrusClient({
   network: "mainnet",
   suiClient,
 });
+
+const tasksClient = new CloudTasksClient();
+const parent = tasksClient.queuePath(
+  process.env.PROJECT_ID || "",
+  process.env.REGION || "",
+  process.env.CLOUD_NAME || ""
+);
 
 const upload = multer({ dest: "uploads/" });
 
@@ -187,10 +188,7 @@ app.post("/process-site", upload.single("file"), async (req, res) => {
   const outputZipPath = path.join(
     __dirname,
     "outputs",
-    `${attributes_data.data["site-name"].replace(
-      /\s+/g,
-      "_"
-    )}_${Date.now()}.zip`
+    `${uuidv4()}_${Date.now()}.zip`
   );
   await fs.ensureDir(path.dirname(outputZipPath));
   const zip = new AdmZip();
@@ -218,6 +216,7 @@ app.post("/process-site", upload.single("file"), async (req, res) => {
   const max_attempts_write_blob = 2;
   const max_attempts_set_status = 5;
   const zipBuffer = await readFile(outputZipPath);
+  console.log("got zipfile");
   while (write_blob_attempts < max_attempts_write_blob) {
     try {
       new_blob_data = await walrusClient.writeBlob({
@@ -227,6 +226,7 @@ app.post("/process-site", upload.single("file"), async (req, res) => {
         signer: keypair,
         attributes: { ...attributes_data.data, forceId: uuidv4() },
       });
+      console.log("wrote blob");
       if (
         new_blob_data.blobObject.certified_epoch ||
         new_blob_data.blobObject.certified_epoch !== null
@@ -340,22 +340,22 @@ app.post("/process-site", upload.single("file"), async (req, res) => {
   let response;
   try {
     const payload = {
-      "arg1": "publish",
-      "arg2": blobObjectId
+      arg1: "publish",
+      arg2: blobObjectId,
     };
 
     [response] = await tasksClient.createTask({
       parent,
       task: {
         httpRequest: {
-          httpMethod: 'POST',
-          url: `https://job-777397487566.asia-southeast1.run.app`,
+          httpMethod: "POST",
+          url: `https://${process.env.JOB_ID}.${process.env.REGION}.run.app`,
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
-          body: Buffer.from(JSON.stringify(payload)).toString('base64'),
+          body: Buffer.from(JSON.stringify(payload)).toString("base64"),
           oidcToken: {
-            serviceAccountEmail: 'cloud-tasks-http-request@ivory-project-457721.iam.gserviceaccount.com',
+            serviceAccountEmail: process.env.SERVICE_ACCOUNT_EMAIL,
           },
         },
       },
@@ -364,8 +364,7 @@ app.post("/process-site", upload.single("file"), async (req, res) => {
     res.status(500).json({
       statusCode: 0,
       error: {
-        error_message:
-          "Failed to create task",
+        error_message: "Failed to create task",
         error_details: error,
       },
     });
@@ -380,6 +379,283 @@ app.post("/process-site", upload.single("file"), async (req, res) => {
     });
   }
 });
+
+// app.post("/process-site", upload.single("file"), async (req, res) => {
+//   if (!req.file) {
+//     res.status(400).json({
+//       statusCode: 0,
+//       error: "No file uploaded",
+//     });
+//     return;
+//   }
+
+//   const zipFile = req.file;
+//   const extractPath = path.join(__dirname, "temp", Date.now().toString());
+//   await fs
+//     .createReadStream(zipFile.path)
+//     .pipe(unzipper.Extract({ path: extractPath }))
+//     .promise();
+
+//   const attributes = req.body.attributes
+//     ? typeof req.body.attributes === "string"
+//       ? JSON.parse(req.body.attributes)
+//       : req.body.attributes
+//     : {};
+//   const attributes_data = inputWriteBlobScheme.safeParse(attributes);
+//   if (!attributes_data.success) {
+//     res.status(400).json({
+//       statusCode: 0,
+//       errors: attributes_data.error.errors.map((err) => ({
+//         error_message: err.message,
+//         error_field: err.path.join("."),
+//       })),
+//     });
+//     return;
+//   }
+
+//   let indexDir: string | null;
+
+//   if (attributes_data.data.is_build === "0") {
+//     const buildDir = await findPackageJsonPath(extractPath);
+//     if (!buildDir) throw new Error("package.json not found");
+//     await new Promise((resolve, reject) => {
+//       exec(attributes_data.data.install_command, { cwd: buildDir }, (err) => {
+//         if (err) return reject(err);
+//         exec(attributes_data.data.build_command, { cwd: buildDir }, (err2) => {
+//           if (err2) return reject(err2);
+//           resolve(true);
+//         });
+//       });
+//     });
+//     indexDir = await findIndexHtmlInKnownDirs(buildDir);
+//     if (!indexDir) {
+//       indexDir = await findIndexHtmlPath(extractPath);
+//     }
+//   } else {
+//     indexDir = await findIndexHtmlPath(extractPath);
+//   }
+
+//   if (!indexDir) throw new Error("index.html not found");
+//   const wsResources = {
+//     headers: {},
+//     routes: {},
+//     metadata: {
+//       link: "https://subdomain.wal.app/",
+//       image_url: "https://www.walrus.xyz/walrus-site",
+//       description: "This is a walrus site.",
+//       project_url: "https://github.com/MystenLabs/walrus-sites/",
+//       creator: "MystenLabs",
+//     },
+//     ignore: ["/private/", "/secret.txt", "/images/tmp/*"],
+//   };
+
+//   await fs.writeJson(path.join(indexDir, "ws-resources.json"), wsResources, {
+//     spaces: 2,
+//   });
+
+//   const outputZipPath = path.join(
+//     __dirname,
+//     "outputs",
+//     `${attributes_data.data["site-name"].replace(
+//       /\s+/g,
+//       "_"
+//     )}_${Date.now()}.zip`
+//   );
+//   await fs.ensureDir(path.dirname(outputZipPath));
+//   const zip = new AdmZip();
+
+//   const addFilesToZip = async (dir: string, baseInZip = "") => {
+//     const entries = await fs.readdir(dir);
+//     for (const entry of entries) {
+//       const fullPath = path.join(dir, entry);
+//       const stat = await fs.stat(fullPath);
+//       if (stat.isDirectory()) {
+//         await addFilesToZip(fullPath, path.join(baseInZip, entry));
+//       } else {
+//         const fileContent = await fs.readFile(fullPath);
+//         zip.addFile(path.join(baseInZip, entry), fileContent);
+//       }
+//     }
+//   };
+
+//   await addFilesToZip(indexDir);
+//   zip.writeZip(outputZipPath);
+
+//   let new_blob_data;
+//   let write_blob_attempts = 0;
+//   const delay = 1000;
+//   const max_attempts_write_blob = 2;
+//   const max_attempts_set_status = 5;
+//   const zipBuffer = await readFile(outputZipPath);
+//   while (write_blob_attempts < max_attempts_write_blob) {
+//     try {
+//       new_blob_data = await walrusClient.writeBlob({
+//         blob: zipBuffer,
+//         deletable: true,
+//         epochs: Number(attributes_data.data.epochs),
+//         signer: keypair,
+//         attributes: { ...attributes_data.data, forceId: uuidv4() },
+//       });
+//       if (
+//         new_blob_data.blobObject.certified_epoch ||
+//         new_blob_data.blobObject.certified_epoch !== null
+//       ) {
+//         break;
+//       }
+//       write_blob_attempts++;
+//       await new Promise((resolve) => setTimeout(resolve, delay));
+//     } catch (error) {
+//       res.status(502).json({
+//         statusCode: 0,
+//         error: {
+//           error_message: "Failed to write blob to Walrus",
+//           error_details: error,
+//         },
+//       });
+//       return;
+//     }
+
+//     if (write_blob_attempts === max_attempts_write_blob) {
+//       let set_status_attempts = 0;
+//       while (set_status_attempts < max_attempts_set_status) {
+//         try {
+//           await walrusClient.executeWriteBlobAttributesTransaction({
+//             blobObjectId: new_blob_data.blobObject.id.id,
+//             signer: keypair,
+//             attributes: { status: "2" },
+//           });
+//           break;
+//         } catch (error) {
+//           set_status_attempts++;
+//           if (set_status_attempts === max_attempts_set_status) {
+//             if (error instanceof Error) {
+//               res.status(502).json({
+//                 statusCode: 0,
+//                 error: {
+//                   error_message:
+//                     "Failed to change status code to 2 in blob attributes",
+//                   error_details: error,
+//                 },
+//               });
+//               return;
+//             }
+//           }
+//           await new Promise((resolve) => setTimeout(resolve, delay));
+//         }
+//       }
+//       res.status(504).json({
+//         statusCode: 0,
+//         error: "Certified epoch is null",
+//       });
+//       return;
+//     }
+//   }
+
+//   if (!new_blob_data) {
+//     res.status(503).json({
+//       statusCode: 0,
+//       error: "WriteBlob success but data is undefined",
+//     });
+//     return;
+//   }
+
+//   const blobObjectId = new_blob_data.blobObject.id.id;
+
+//   try {
+//     await walrusClient.executeWriteBlobAttributesTransaction({
+//       blobObjectId: blobObjectId,
+//       signer: keypair,
+//       attributes: { blobId: new_blob_data.blobId },
+//     });
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       res.status(502).json({
+//         statusCode: 0,
+//         error: {
+//           error_message:
+//             "Failed to execute write blob attributes transaction to Walrus",
+//           error_details: error,
+//         },
+//       });
+//       return;
+//     }
+//   }
+
+//   let check_blob_id;
+
+//   try {
+//     check_blob_id = await walrusClient.readBlobAttributes({
+//       blobObjectId: blobObjectId,
+//     });
+//   } catch (error) {
+//     res.status(502).json({
+//       statusCode: 0,
+//       error: {
+//         error_message: "Failed to read blob attributes from Walrus",
+//         error_details: error,
+//       },
+//     });
+//     return;
+//   }
+
+//   if (!check_blob_id || !check_blob_id.blobId) {
+//     res.status(502).json({
+//       statusCode: 0,
+//       error: "Failed to add blobId to attributes",
+//     });
+//     return;
+//   }
+
+//   let response;
+//   try {
+//     const payload = {
+//       "arg1": "publish",
+//       "arg2": blobObjectId
+//     };
+
+//     [response] = await tasksClient.createTask({
+//       parent,
+//       task: {
+//         httpRequest: {
+//           httpMethod: 'POST',
+//           url: `https://job-777397487566.asia-southeast1.run.app`,
+//           headers: {
+//             'Content-Type': 'application/json',
+//           },
+//           body: Buffer.from(JSON.stringify(payload)).toString('base64'),
+//           oidcToken: {
+//             serviceAccountEmail: 'cloud-tasks-http-request@ivory-project-457721.iam.gserviceaccount.com',
+//           },
+//         },
+//       },
+//     });
+//   } catch (error) {
+//     res.status(500).json({
+//       statusCode: 0,
+//       error: {
+//         error_message:
+//           "Failed to create task",
+//         error_details: error,
+//       },
+//     });
+//     return;
+//   }
+
+//   if (response.name) {
+//     res.status(200).json({
+//       statusCode: 1,
+//       objectId: blobObjectId,
+//       taskName: `Created task ${response.name}`,
+//     });
+//   }
+
+//   // Delete the uploaded zip file
+//   try {
+//     await fs.unlink(zipFile.path);
+//   } catch (error) {
+//     console.error("Failed to delete zip file:", error);
+//   }
+// });
 
 app.put("/set-attributes", async (req, res) => {
   const object_id = req.query.object_id;
@@ -657,7 +933,7 @@ app.put("/update-blob-n-run-job", upload.single("file"), async (req, res) => {
     await walrusClient.executeWriteBlobAttributesTransaction({
       blobObjectId: blobObjectId,
       signer: keypair,
-      attributes: attributesUpdate
+      attributes: attributesUpdate,
     });
   } catch (error) {
     if (error instanceof Error) {
@@ -690,46 +966,45 @@ app.put("/update-blob-n-run-job", upload.single("file"), async (req, res) => {
     return;
   }
 
-  const url = `https://run.googleapis.com/v2/projects/${process.env.PROJECT_ID}/locations/${process.env.REGION}/jobs/${process.env.CLOUD_RUN_JOB_NAME}:run`;
-  const client = await auth.getClient();
-  const token = await client.getAccessToken();
-
   let response;
   try {
-    response = await axios.post(
-      url,
-      {
-        overrides: {
-          containerOverrides: [
-            {
-              args: ["publish", new_blob_data.blobObject.id.id],
-            },
-          ],
+    const payload = {
+      arg1: "publish",
+      arg2: blobObjectId,
+    };
+
+    [response] = await tasksClient.createTask({
+      parent,
+      task: {
+        httpRequest: {
+          httpMethod: "POST",
+          url: `https://${process.env.JOB_ID}.${process.env.REGION}.run.app`,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: Buffer.from(JSON.stringify(payload)).toString("base64"),
+          oidcToken: {
+            serviceAccountEmail: process.env.SERVICE_ACCOUNT_EMAIL,
+          },
         },
       },
-      {
-        headers: {
-          Authorization: `Bearer ${token.token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    });
   } catch (error) {
     res.status(500).json({
       statusCode: 0,
       error: {
-        error_message:
-          "Failed to triggering Cloud Run job to update walrus-site",
+        error_message: "Failed to create task",
         error_details: error,
       },
     });
     return;
   }
 
-  if (response.status == 200) {
+  if (response.name) {
     res.status(200).json({
       statusCode: 1,
-      details: response.data,
+      objectId: blobObjectId,
+      taskName: `Created task ${response.name}`,
     });
   }
 });
@@ -778,44 +1053,44 @@ app.delete("/delete-site", async (req, res) => {
     }
   }
 
-  const url = `https://run.googleapis.com/v2/projects/${process.env.PROJECT_ID}/locations/${process.env.REGION}/jobs/${process.env.CLOUD_RUN_JOB_NAME}:run`;
-  const client = await auth.getClient();
-  const token = await client.getAccessToken();
   let response;
   try {
-    response = await axios.post(
-      url,
-      {
-        overrides: {
-          containerOverrides: [
-            {
-              args: ["delete_site", attributes_data.data.object_id],
-            },
-          ],
+    const payload = {
+      arg1: "delete_site",
+      arg2: object_id,
+    };
+
+    [response] = await tasksClient.createTask({
+      parent,
+      task: {
+        httpRequest: {
+          httpMethod: "POST",
+          url: `https://${process.env.JOB_ID}.${process.env.REGION}.run.app`,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: Buffer.from(JSON.stringify(payload)).toString("base64"),
+          oidcToken: {
+            serviceAccountEmail: process.env.SERVICE_ACCOUNT_EMAIL,
+          },
         },
       },
-      {
-        headers: {
-          Authorization: `Bearer ${token.token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    });
   } catch (error) {
     res.status(500).json({
       statusCode: 0,
       error: {
-        error_message:
-          "Failed to triggering Cloud Run job to delete walrus-site",
+        error_message: "Failed to create task",
         error_details: error,
       },
     });
     return;
   }
-  if (response.status == 200) {
+
+  if (response.name) {
     res.status(200).json({
       statusCode: 1,
-      details: response.data,
+      taskName: `Created task ${response.name}`,
     });
   }
 });
@@ -864,44 +1139,44 @@ app.put("/add-site-id", async (req, res) => {
     }
   }
 
-  const url = `https://run.googleapis.com/v2/projects/${process.env.PROJECT_ID}/locations/${process.env.REGION}/jobs/${process.env.CLOUD_RUN_JOB_NAME}:run`;
-  const client = await auth.getClient();
-  const token = await client.getAccessToken();
   let response;
   try {
-    response = await axios.post(
-      url,
-      {
-        overrides: {
-          containerOverrides: [
-            {
-              args: ["get_site_id", attributes_data.data.object_id],
-            },
-          ],
+    const payload = {
+      arg1: "get_site_id",
+      arg2: object_id,
+    };
+
+    [response] = await tasksClient.createTask({
+      parent,
+      task: {
+        httpRequest: {
+          httpMethod: "POST",
+          url: `https://${process.env.JOB_ID}.${process.env.REGION}.run.app`,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: Buffer.from(JSON.stringify(payload)).toString("base64"),
+          oidcToken: {
+            serviceAccountEmail: process.env.SERVICE_ACCOUNT_EMAIL,
+          },
         },
       },
-      {
-        headers: {
-          Authorization: `Bearer ${token.token}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    });
   } catch (error) {
     res.status(500).json({
       statusCode: 0,
       error: {
-        error_message:
-          "Failed to triggering Cloud Run job to delete walrus-site",
+        error_message: "Failed to create task",
         error_details: error,
       },
     });
     return;
   }
-  if (response.status == 200) {
+
+  if (response.name) {
     res.status(200).json({
       statusCode: 1,
-      details: response.data,
+      taskName: `Created task ${response.name}`,
     });
   }
 });
